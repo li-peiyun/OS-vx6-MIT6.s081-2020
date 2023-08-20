fdc73d8c7b14bb469c9709fe4c931ec8fbda867a
li-peiyun <104260014+li-peiyun@users.noreply.github.com>
2023-08-20 20:30:48 +0800

    net

--- kernel/sprintf.d
+++
@@ -1,4 +0,0 @@
-kernel/sprintf.o: kernel/sprintf.c \
- /usr/lib/gcc-cross/riscv64-linux-gnu/9/include/stdarg.h kernel/types.h \
- kernel/param.h kernel/spinlock.h kernel/sleeplock.h kernel/fs.h \
- kernel/file.h kernel/riscv.h kernel/defs.h
--- user/pingpong.c
+++ user/pingpong.c
@@ -1,42 +1,52 @@
 #include "kernel/types.h"
+#include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]) {
-    int pid;
-    // two-way delivery needs two pipes 
-    int pipe1[2], pipe2[2];
-    char buf[] = {'a'};
-    // pipe
-    pipe(pipe1);
-    pipe(pipe2);
-    // create child proccess
-    int ret = fork();
+#define N 5
+char buf[N];
 
-    // parent sends in pipe1[1], child receives in pipe1[0]
-    // child sends in pipe2[1], parent receives in pipe2[0]
-    
-    if (ret == 0) {
-        // it is child process
-        // receives in pipe1[0]
-        // sends in pipe2[1]
-        pid = getpid();
-        close(pipe1[1]);
-        close(pipe2[0]);
-        read(pipe1[0], buf, 1);
-        printf("%d: received ping\n", pid);
-        write(pipe2[1], buf, 1);
-        exit(0);
-    }
-    else {
-        // it is parent process
-        // sends in pipe1[1]
-        // receives in pipe2[0]
-        pid = getpid();
-        close(pipe1[0]);
-        close(pipe2[1]);
-        write(pipe1[1], buf, 1);
-        read(pipe2[0], buf, 1);
-        printf("%d: received pong\n", pid);
-        exit(0);
-    }
+void
+pong(int *parent_to_child, int *child_to_parent) {
+  if (read(parent_to_child[0], buf, N) < 0) {
+    printf("read failed\n");
+  }
+  printf("%d: received %s\n", getpid(), buf);
+  if (write(child_to_parent[1], "pong", 4) != 4) {
+    printf("write failed\n");
+  }
+}
+
+void
+ping(int *parent_to_child, int *child_to_parent) {
+  
+  if (write(parent_to_child[1], "ping", 4) != 4) {
+    printf("write failed\n");
+  }
+  if (read(child_to_parent[0], buf, N) < 0) {
+    printf("read failed\n");
+  }
+  printf("%d: received %s\n", getpid(), buf);
+}
+
+int
+main(int argc, char *argv[])
+{
+  int parent_to_child[2];
+  int child_to_parent[2];
+
+  int pid;
+
+  if (pipe(parent_to_child) < 0 || pipe(child_to_parent) < 0) {
+    printf("pipe failed\n");
+  }
+  if ((pid = fork()) < 0) {
+    printf("fork failed\n");
+  }
+  if (pid == 0) {
+    pong(parent_to_child, child_to_parent);
+  } else {
+    ping(parent_to_child, child_to_parent);
+  }
+  
+  exit(0);
 }
